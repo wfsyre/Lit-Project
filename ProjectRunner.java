5eca1c7d8535e15a9da96459072f03a82549164a
William Syre <wfsyre@gmail.com>
2016-04-22 21:43:11 -0400

    Added all pieces

--- ProjectRunner.java
+++ ProjectRunner.java
@@ -19,19 +19,23 @@ public class ProjectRunner extends Application {
         StoryNode goldenTornadoes = new StoryNode("Golden Tornadoes", start, "Golden Tornadoes", true, true);
         StoryNode tribBook = new StoryNode("Tribute Book", goldenTornadoes, "", false, false);
         StoryNode puzz4 = new StoryNode("Physics Building", null, "", false, true);
-        StoryNode olympics = new StoryNode("Building Site", puzz4, "Howey", true, true);
         StoryNode gildedStorms = new StoryNode("Gilded Storms", goldenTornadoes, "faculty", true, true);
         StoryNode dist = new StoryNode("Distinguished Faculty", start, "Howey", true, true);
         StoryNode pGE = new StoryNode("Project Gold Experience", start, "Edwards", true, true);
+        StoryNode fOB = new StoryNode("Fall of Babel", pGE, "Glory Hound", true, true);
+        StoryNode lll = new StoryNode("Location Location Location", fOB, "", false, true);
+        StoryNode end = new StoryNode("1996", start, "", false, true);
+        
+        
         //Tribute Images
-        DocNode t1 = new DocNode("Football Season", "1928 Fball Season", "image", false);
+        DocNode t1 = new DocNode("Football Season", "1928 Fball Season.jpg", "image", false);
         DocNode t2 = new DocNode("Fan mail", "Bae Watch.jpg", "image", false);
         DocNode t3 = new DocNode("Burton and Bary","Burton and Bary.jpg","image", false);
         DocNode t4 = new DocNode("Coach Alexander","Coach Alexander.jpg","image", false);
         DocNode t5 = new DocNode("Grant Field","Grant Field.jpg","image", false);
-        DocNode t6 = new DocNode("Faces 1","Important Faces 1","image", false);
-        DocNode t7 = new DocNode("Faces 2","Important Faces 2","image", false);
-        DocNode t8 = new DocNode("Faces 3","Important Faces 3","image", false);
+        DocNode t6 = new DocNode("Faces 1","Important Faces 1.jpg","image", false);
+        DocNode t7 = new DocNode("Faces 2","Important Faces 2.jpg","image", false);
+        DocNode t8 = new DocNode("Faces 3","Important Faces 3.jpg","image", false);
         DocNode t9 = new DocNode("Overhead","Overhead.jpg","image", false);
         DocNode t10 = new DocNode("Praise","Praise.jpg","image", false);
         DocNode t11 = new DocNode("Credits","Printings By.jpg","image", false);
@@ -49,19 +53,30 @@ public class ProjectRunner extends Application {
         DocNode teamImage = new DocNode("Tribute Book Team", "Team Info.jpg",
                 "image", false);
         //Documents
+        DocNode sec1 = new DocNode("Bill and Bill", "Bill and Bill.jpg", "image", false);
+        DocNode sec2 = new DocNode("Bob Lang", "Bob Lang 1.jpg", "image", false);
+        DocNode sec3 = new DocNode("Bob Lang 2", "Bob Lang 2.jpg", "image", false);
+        DocNode sec4 = new DocNode("Bob Lang 2.5", "Bob Lang2.5.jpg", "image", false);
+        DocNode investigate = new DocNode("The End", "Text/Final Reveal.txt", "text", false);
+        DocNode finalHints = new DocNode("The End?", "Text/last puzzle hints.txt", "text", false);
+        DocNode rsKing = new DocNode("King's Reflection", "Text/R. S. King Journal.txt", "text", false);
+        DocNode centroids = new DocNode("Centroid Notes", "centroid notes.jpg", "text", true, "Swarm");
+        DocNode eAll3 = new DocNode("Edward to All 3", "Request WEC 2.jpg", "image", false);
+        DocNode eAll2 = new DocNode("Edward to All 2", "Request WEC 1.jpg", "image", false);
+        DocNode eAll1 = new DocNode("Edward to All 1", "Request SLC.jpg", "image", false);
         DocNode ek1 = new DocNode("Edward-King 1", "Request King 1.jpg", "image", false);
         DocNode ek2 = new DocNode("Edward-King 2", "Request King 2.jpg", "image", false);
         DocNode ke1 = new DocNode("King-Edward", "King Fights Back.jpg", "image", false);
         DocNode excel = new DocNode("Current Research", "ResearchPaperArchive.xlsx", "excel", false);
         DocNode uL1 = new DocNode("Underling's log #1", "Text/Assistant Log 1.txt", "text", false);
-        DocNode uL2 = new DocNode("Underling's log #2", "Text/Assistant Log 2.txt", "text", false);
+        DocNode uL2 = new DocNode("Underling's log #2", "Text/Assistant Log 2.txt", "text", true, "King");
         DocNode readMe = new DocNode("README", "Text/README.txt", "text", false);
         DocNode gfAudio = new DocNode("Interview", "test.mp3", "audio", false);
         DocNode technique = new DocNode("technique", "Text/technique.txt", "text", true, "John Doe");
         DocNode startDoc4 = new DocNode("unencrypt.exe", "Text/puzz4start.txt", "text", false);
-        DocNode campusMapHint = new DocNode("Location", "Text/location.txt", "text", true, "faculty");
+        DocNode campusMapHint = new DocNode("Location", "Text/location.txt", "text", false);
         DocNode goldQuestion = new DocNode("Golden Question",
-                        "Golden Question.txt", "text", false);
+                        "Text/Golden Question.txt", "text", false);
         
         start.addDoc(0, gfAudio);
         start.addDoc(1, technique);
@@ -96,11 +111,25 @@ public class ProjectRunner extends Application {
         puzz4.addDoc(1, campusMapHint);
         dist.addDoc(0, excel);
         dist.addFolder(1, pGE);
-        pGE.addDoc(0, uL1);
-        pGE.addDoc(1, ek1);
-        pGE.addDoc(2, ek2);
-        pGE.addDoc(3, ke1);
-        pGE.addDoc(4, uL2);
+        pGE.addDoc(0, eAll1);
+        pGE.addDoc(1, eAll2);
+        pGE.addDoc(2, eAll3);
+        pGE.addDoc(3, uL1);
+        pGE.addDoc(4, ek1);
+        pGE.addDoc(5, ek2);
+        pGE.addDoc(6, ke1);
+        pGE.addDoc(7, uL2);
+        pGE.addFolder(8, fOB);
+        fOB.addDoc(0, rsKing);
+        fOB.addDoc(1, centroids);
+        end.addDoc(0, sec1);
+        end.addDoc(1, sec2);
+        end.addDoc(2, sec3);
+        end.addDoc(3, sec4);
+        //1 more here
+        end.addDoc(4,  investigate);
+        lll.addDoc(0, finalHints);
+        
         
         
         String answer = "";
@@ -124,6 +153,7 @@ public class ProjectRunner extends Application {
                 		String userPass = scan.nextLine();
                 		if (userPass.equals(story.getPass())) {
                 			System.out.println("Success");
+                			story.setIsLocked(false);
                 			current = story;
                 			path = path.substring(0, path.length() - 1) + "\\" + current.getName() + ">";
                 		} else {
